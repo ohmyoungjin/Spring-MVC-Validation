08f218bf5f9c3ff9df60b130d43b16f1d80744f1
softbridge <softbridgedev@gmail.com>
2022-11-17 13:17:28 +0900

    20221117 :: Spring-MVC-Validation Validation addForm.html edit

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -16,6 +17,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
+@Slf4j
 public class ValidationItemControllerV1 {
 
     private final ItemRepository itemRepository;
@@ -67,6 +69,7 @@ public class ValidationItemControllerV1 {
 
         //검증에 실패하면 다시 입력 폼으로
         if (!errors.isEmpty()) {
+            log.info("errors = {} ", errors);
             model.addAttribute("errors", errors);
             return "validation/v1/addForm";
         }
